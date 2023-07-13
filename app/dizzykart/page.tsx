28af2bd75d88c7e84c86004a29aca9a7b59ff5c1
imran <imran>
2023-07-13 07:43:09 +0530

    Updated

---
+++ app/dizzykart/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import product from './product';
+import Link from 'next/link';
+
+const page = () => {
+  return (
+    <div>React Router
+
+      
+    </div>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { AppDispatch, useAppSelector } from '@/redux/store'
 import {useDispatch} from "react-redux";
 import { addUserName } from '@/redux/slicer/registerSlice';
 import App from 'next/app';
+import Link from 'next/link';
 
 
 
@@ -19,9 +20,10 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div onClick={setUserName} >
+        <div >
              React APP fragment
               <h1>{userName}</h1>
+              <Link href="/dizzykart">Product</Link>
         </div>
     </main>
   )
